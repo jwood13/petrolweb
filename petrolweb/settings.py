d2eade8f6f4019463ce2567614a1f5619ac217b5
jared Wood <jared.wood@sydney.edu.au>
2021-09-23 14:49:14 +1000

    Debug off

--- petrolweb/settings.py
+++ petrolweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6*(vc4d@30-g670r%hb_ch&hph*^48()m1i^@mm2+b1!+=ik9s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
