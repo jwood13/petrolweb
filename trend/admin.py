9380f8192123a1f0b3ed6e455a012e35e43a0e8c
jared Wood <jared.wood@sydney.edu.au>
2021-11-04 15:22:27 +1100

    Register fuel_prices in admin

--- trend/admin.py
+++ trend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Station
+from .models import Fuel_Price, Station
 
 admin.site.register(Station)
+admin.site.register(Fuel_Price)
