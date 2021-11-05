724b01544ba67734b7ce66da5b252b5fe75b1dc6
jared Wood <jared.wood@sydney.edu.au>
2021-11-05 12:04:56 +1100

    Add security and database for deploy
    Load secret key from os
    use sqlite for local, postgresql for heroku on deploy

--- petrolweb/settings.py
+++ petrolweb/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import psycopg2
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6*(vc4d@30-g670r%hb_ch&hph*^48()m1i^@mm2+b1!+=ik9s'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jwpetrol.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['jwpetrol.herokuapp.com']
+
+if DEBUG:
+    ALLOWED_HOSTS.append('127.0.0.1')
 
 
 # Application definition
@@ -75,12 +81,18 @@ WSGI_APPLICATION = 'petrolweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
 
 
 # Password validation
@@ -132,3 +144,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security settings for Deploy
+# https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
