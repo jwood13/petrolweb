9bc4b6a805e470dcbf6d066b237b168cbae35a02
jared Wood <jared.wood@sydney.edu.au>
2022-02-16 11:53:16 +1100

    Update admin for easier analysis of data

--- trend/admin.py
+++ trend/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Fuel_Price, Station
 
-admin.site.register(Station)
-admin.site.register(Fuel_Price)
+
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'postcode', 'state')
+
+
+class Fuel_PriceAdmin(admin.ModelAdmin):
+    list_display = ('station', 'fuel', 'price', 'time')
+    list_filter = ['time']
+
+
+admin.site.register(Station, StationAdmin)
+admin.site.register(Fuel_Price, Fuel_PriceAdmin)
