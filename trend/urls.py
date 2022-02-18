01263bc6166c2a6d5fbea2424c97aa67df82f9f1
jared Wood <jared.wood@sydney.edu.au>
2022-02-18 16:06:20 +1100

    Add a default page for /id,
    intended to hold current prices later

--- trend/urls.py
+++ trend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('update_prices', views.get_all_data, name='get_data'),
     path('register_stations', views.register_stations, name='register_stations'),
+    path('<int:station_id>', views.station_history, name='current_station'),
     path('history/<int:station_id>', views.station_history, name='station_history')
 
 
